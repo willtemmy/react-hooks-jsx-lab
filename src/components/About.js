9c7c7ad5a7d2807e3245e473c4e4c253b8b8a164
William Mtemi Mponzi <willbrian009@gmail.com>
2023-04-20 12:58:32 +0300

    complete

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,7 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about"> <h2>About me</h2 ><p>Done</p><img src={image} alt="I made this"></img></div>;
 }
 
 export default About;
